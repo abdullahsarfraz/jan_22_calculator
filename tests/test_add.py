22d5cb79cb9b8847b3569561a669ff6721ec3f7a
abdullahsarfraz <sarfraza@sheridancollege.ca>
2019-01-22 20:58:23 -0500

    Make parameters optional for add function

--- tests/test_add.py
+++ tests/test_add.py
@@ -8,3 +8,13 @@ def test_two_plus_two():
     if given 2 and 2 as parmeters, 4 shoud be returned
     """
     assert add(2, 2) == 4
+
+def test_three_and_three():
+    """
+    if given 3 and 3, 6 should be returned
+    """
+    assert add(3, 3) == 6
+
+def test_no_parameters():
+
+    assert add() == 0
